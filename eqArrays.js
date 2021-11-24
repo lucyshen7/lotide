000abf49a0f2221bee2c61256ce0e7b3d83d534b
Lucy Shen <lucyshen77@gmail.com>
2021-11-24 05:34:35 +0000

    Implemented eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+/* Pseudocode
+- Compare each element in two arrays for a perfect match.
+*/
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let output = ""; 
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      output = true; // Assign a truthy value to output if matched
+    } else {
+      output = false; // Assign a falsey value to output if not matched
+    };
+  };    
+  return output;
+};
+
+// TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should FAIL
