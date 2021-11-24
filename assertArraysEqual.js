5aa4f5ce8af0e3639a8db1872f5d95d5bbdbbc08
Lucy Shen <lucyshen77@gmail.com>
2021-11-24 06:22:02 +0000

    Implemented assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+
+/*
+Pseudocode:
+- assertArraysEqual will take two arrays and console.log an appropriate message to the console.
+*/
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let output = "";
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      output = true; // Assign a truthy value to output if matched
+    } else {
+      output = false; // Assign a falsey value to output if not matched
+    }
+  }
+  return output;
+};
+
+// Implement assertArraysEqual for asserting that two arrays are equal
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+
+};
+
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [1, 2, "3"]); // => should FAIL
