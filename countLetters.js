23717557ca5d06a6e8fd143f58451c07befdcc97
Lucy Shen <lucyshen77@gmail.com>
2021-11-25 02:56:45 +0000

    implemented countLetters

---
+++ countLetters.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// countLetters
+
+const countLetters = function(string) {
+  let obj = {};
+
+  let str = string.split(" ").join(""); // Remove spaces in string
+  // console.log(str)
+
+  for (const letter of str) { // Loop through each letter of str
+    if (obj[letter]) { // If the key exists in the obj, increment value by 1
+      obj[letter] += 1;
+    } else { // If the key does not exist, set value to 1
+      obj[letter] = 1; 
+    }
+  }
+  return obj;
+}
+
+// TEST CODE
+let obj1 = {};
+obj1 = (countLetters("lighthouse in the house"));
+
+assertEqual(obj1["h"], 4);
+assertEqual(obj1["e"], 3);
+assertEqual(obj1["n"], 1);
