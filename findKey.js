960806b760ecf3d8e3c5e5954bedc4ecafb9b240
Lucy Shen <lucyshen77@gmail.com>
2021-11-26 00:52:34 +0000

    implemented findKey

---
+++ findKey.js
@@ -0,0 +1,54 @@
+// copied over assertEqual
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Implement findKey
+// - Takes in an object and a callback
+// - Scans object and returns the first key for which the callback returns a truthy value
+// - If no key is found, then it should return undefined
+
+const findKey = function(object, callBack) {
+  let result;
+  for (let key in object) {
+    if (callBack(object[key])) {
+      result = key;
+      return result;
+    }
+  }
+  return result;
+};
+
+// TEST CODE
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5), undefined);
