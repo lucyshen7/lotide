a5861e168762fd1de58990fda46ea8d57f4a06d6
Lucy Shen <lucyshen77@gmail.com>
2021-11-29 21:18:30 +0000

    Incorporated Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,21 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// TEST CODE
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 5 for [5,6,7]]", () => {
+    assert.strictEqual(head([5,6,7]), 5); 
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+
+});
