2ed59314d5de05afb94217d0c0dddf39692a19ef
Lucy Shen <lucyshen77@gmail.com>
2021-11-29 16:31:31 +0000

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 /* Pseudocode
 - Compare each element in two arrays for a perfect match.
@@ -22,12 +16,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
   return output;
 };
 
-// TEST CODE
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should FAIL
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,12 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should FAIL
